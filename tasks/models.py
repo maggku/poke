c3ebb18155420c5d17b3bca599930f545cdd7d1b
maggku <maggie.kuczynska@gmail.com>
2026-05-06 11:48:30 +0100

    Add models in tasks/models.py

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+# Models for an MVP version of the project
+
+class Email(models.Model):
+    sender = models.EmailField()
+    subject = models.TextField()
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.subject
+
+
+class Task(models.Model):
+    email = models.ForeignKey(Email, on_delete=models.CASCADE)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.description
