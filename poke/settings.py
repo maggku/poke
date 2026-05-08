0daa6b3486f23a869bd14273b5b25490f380c91d
maggku <maggie.kuczynska@gmail.com>
2026-05-08 13:53:09 +0100

    Add Anthropic API link in settings.py, adjust poke/urls.py and tasks/urls.py

--- poke/settings.py
+++ poke/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
+
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
+ANTHROPIC_API_KEY = os.environ.get('ANTHROPIC_API_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
