cccb300772c1a285f2428e9ee2af9193720fc3c6
maggku <maggie.kuczynska@gmail.com>
2026-07-14 12:23:08 +0100

    Update tasks/urls.py

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.tasks_display, name='tasks'),
+    path('tasks/<int:email_id>/', views.email_detail, name='email_detail'),
+    path('tasks/<int:email_id>/generate/', views.generate_task_from_email, name='generate_task_from_email'),
 ]
